6194698aba970a5cd07c6c3d00e908442a358bcb
Britt Carter <brittbmh@gmail.com>
2019-04-21 19:56:22 -0500

    add reducer index

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,16 @@
+import { combineReducers } from 'redux';
+import errors from './errorsReducer';
+import loginMode from './loginModeReducer';
+import user from './userReducer';
+
+// This is imported in index.js as rootSaga
+
+// Lets make a bigger object for our store, with the objects from our reducers.
+// This is what we get when we use 'state' inside of 'mapStateToProps'
+const rootReducer = combineReducers({
+    errors, // contains registrationMessage and loginMessage
+    loginMode, // will have a value of 'login' or 'registration' to control which screen is shown
+    user, // will have an id and username if someone is logged in
+});
+
+export default rootReducer;
