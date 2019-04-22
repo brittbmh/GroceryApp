6826f3029826ce388462bf794e20d0b5f49ec238
Britt Carter <brittbmh@gmail.com>
2019-04-21 19:53:25 -0500

    add saga index and user saga

---
+++ src/redux/saga/index.js
@@ -0,0 +1,17 @@
+import { all } from 'redux-saga/effects';
+import loginSaga from './loginSaga';
+import registrationSaga from './registrationSaga';
+import userSaga from './userSaga';
+
+// This is imported in index.js as rootSaga
+
+// some sagas trigger other sagas, as an example
+// the registration triggers a login
+// and login triggers setting the user
+export default function* rootSaga() {
+    yield all([
+        loginSaga(),
+        registrationSaga(),
+        userSaga(),
+    ]);
+}
