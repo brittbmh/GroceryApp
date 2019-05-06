db12385c3aeaac5d576da5472d7365246b5b0565
Britt Carter <brittbmh@gmail.com>
2019-05-06 15:46:52 -0500

    add about copy displayed in css box

--- src/components/About/AboutPage.js
+++ src/components/About/AboutPage.js
@@ -5,11 +5,16 @@ import React from 'react';
 
 const AboutPage = () => (
     <div>
-        <div>
+        <div className="about-div">
+            <article>
             <p>
-                Thanks to everyone who helped me with this app!
-      </p>
-            <p>Tech Used: React.js, Node.js/Express.js, AJAX/Axios, Redux.js, PostgreSQL, Sweetalert.js, HTML, CSS </p>
+                Ever get to the store and see strawberry jam on your list, 
+                only to be faced with ten different brands and no idea which one
+                is the preferred one for your household?
+                SSA will help you track the brands and types of goods preferred
+                by your family/household and easily look these up while you are shopping.
+            </p>
+            </article>
         </div>
     </div>
 );
